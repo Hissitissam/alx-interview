4f929b0498b3812a99384fcbe3a8e57314bb1e24
Hissitissam <tissam.hisssi@gmail.com>
2024-08-14 14:18:33 -0700

    first commit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
 import sys
 
+total_file_size = 0
+code = 0
+counter = 0
+status_code_counts = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
 
 def print_msg(dict_sc, total_file_size):
     """Prints the current statistics: total file size and status code counts.
@@ -16,18 +27,6 @@ def print_msg(dict_sc, total_file_size):
         if val != 0:
             print("{}: {}".format(key, val))
 
-
-total_file_size = 0
-code = 0
-counter = 0
-status_code_counts = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
 
 try:
     for line in sys.stdin:
